efb410cd1560a28f4b17f99842eff266ede2f622
MY-2357 <wsmt.zmy@gmail.com>
2025-10-14 19:18:28 +0800

    Con task2:finished

--- src/task_2.cpp
+++ src/task_2.cpp
@@ -5,12 +5,14 @@
 #include "io/gimbal/gimbal.hpp"
 #include "tasks/auto_aim/solver.hpp"
 #include "tasks/auto_aim/yolo.hpp"
+#include "tools/exiter.hpp"
 #include "tools/img_tools.hpp"
 #include "tools/logger.hpp"
 #include "tools/math_tools.hpp"
+#include "tools/pid.hpp"
 #include "tools/plotter.hpp"
 #include "tools/recorder.hpp"
-#include "tools/exiter.hpp"
+#include "tools/trajectory.hpp"
 
 const std::string keys =
   "{help h usage ? | | 输出命令行参数说明}"
@@ -43,13 +45,121 @@ int main(int argc, char * argv[])
   Eigen::Quaterniond q;
   std::chrono::steady_clock::time_point t;
 
+  /*
+    ../tools/中的各个模块分析
+    crc.hpp: CRC校验算法，用于校验数据传输是否正确
+    extended_kalman_filter.hpp : 扩展卡尔曼滤波器
+    img_tools.hpp：图像处理工具，实现在图像上绘点、输出文字
+    logger.hpp：向控制台输出日志，同时保存到文件
+    math_tools.hpp：实现坐标系的转换与其他一些常见数学计算
+    pid.hpp->PID: PID控制算法,实现稳定控制机器人的运动
+    ransac_sine_fitter.hpp->RansacSineFitter：正弦函数拟合器，从充满噪声的点集中估计出一条最佳拟合曲线
+    recorder.hpp->Recorder：根据连续传入的图片按照制定的帧率录制视频，同时将每帧对应的姿态数据（四元数）保存到文件
+    thread_pool.hpp->ThreadPool：线程池，用于并行执行任务
+    thread_pool.hpp->OrderedQueue:实现一个按顺序输出的队列，即使输入乱序，也能按 id 顺序输出。
+    thread_safe_queue.hpp->ThreadSafeQueue：模板类，提供线程安全的生产者-消费者队列
+    trajectory.hpp->Trajectory：用于计算在考虑重力加速度，忽略空气阻力的情况下对象所需要的条件
+  */
+
+  /*
+    上述的模块的应用分析：
+    pid.hpp->PID：
+      实现稳定控制机器人的运动，在任何控制机器人运动的地方都要想着使用它。
+    thread_pool.hpp->OrderedQueue：
+      在相机图像流（或YOLO检测结果）中，帧可能乱序到达：
+      OrderedQueue 负责重新排序；
+      让后续模块始终按正确的时间顺序处理帧。
+  */
+
+  /*
+    根据要求，依次进行以下步骤：
+    1、通过与task_1类似的方式获取当下装甲板的位姿（世界坐标系下）
+    2、考虑重力加速度，使用Trajectory计算云台的合适朝向
+    3、使用PID操纵云台
+    4、反复循环进行以上步骤，当云台到达目标位置并且保持稳定后，进行射击，结束循环，从1、再次开始，累计射击10次
+  */
+
+  /*
+    调试时需要做的是：
+    1、修改满足射击条件的判断
+  */
+  const int shoot_total = 10;  //根据要求，射击进行10次
+  int shoot_count = 0;   //当前已经完成的射击次数
+
   while (!exiter.exit()) {
     // Your code start
 
+    /************下面的代码与task_1相同************/
+    // 打开相机并读取图像
+    camera.read(img, t);
+    // 使用YOLO来检测并获取装甲板的位置（像素坐标系，包括四个点）
+    auto armors = yolo.detect(img);
+    if (armors.empty())  // 没有检测到装甲板
+    {
+      // 让线程休眠，减少资源占用
+      std::this_thread::sleep_for(100ms);
+      continue;
+    }
+    auto_aim::Armor armor = armors.front();  //只跟随第一个装甲板
+    // Solver计算armor的位置
+    q = gimbal.q(t);               //获取当前时间戳下云台的姿态
+    solver.set_R_gimbal2world(q);  // 使用从C板获取的四元数来对solver计算的世界坐标加以修正
+    solver.solve(armor);
+    //计算pitch和yaw
+    auto pos = armor.xyz_in_world;  // 装甲板的中心位置
+    /*
+      这里使用了一个假设：yaw，pitch的取值是负pi到pi，
+      yaw是从x开始，以z为转轴转动逆时针的角度，pitch是从x开始，以y为转轴转动逆时针的角度
+    */
+    float yaw_target = std::atan2(pos.y(), pos.x());
+    float pitch_target = -std::atan2(pos.z(), pos.x());
+    // 这是发送并且记录控制指令的环节，这个地方常用，而且很容易出问题，故而使用lambda表达式单独列出
+    // 修改时，需要同时修改另外两个文件的对应函数
+    auto send_command = [&gimbal, &plotter](
+                          int yaw_target, int pitch_target, bool fire = false) -> void {
+      //使用PID控制算法发送指令
+      tools::PID pid_yaw(0.01f, 5.0f, 0.0f, 0.5f, 5.0f, 0.2f, true);
+      tools::PID pid_pitch(0.01f, 5.0f, 0.0f, 0.5f, 5.0f, 0.2f, true);
+      auto state = gimbal.state();  // 当前云台状态
+      float yaw_output = pid_yaw.calc(yaw_target, state.yaw);
+      float pitch_output = pid_pitch.calc(pitch_target, state.pitch);
+      gimbal.send(true, fire, state.yaw + yaw_output, state.pitch + pitch_output);
+      // 使用plotter绘制向云台发送的控制信息
+      nlohmann::json data;
+      data["yaw"] = state.yaw + yaw_output;
+      data["pitch"] = state.pitch + pitch_output;
+      plotter.plot(data);
+    };
+    /*****************上面代码与task_1相同************/
+    // 计算云台的合适朝向
+    tools::Trajectory trajectory(   //备注：这一行可能会出错的地方：我们认为pos.z()是对应的目标与跑口的相对高度，但实际上我们并不难肯定跑口处的z值为0
+      gimbal.state().bullet_speed, sqrt(pos.x() * pos.x() + pos.y() * pos.y()), pos.z());
+    if (trajectory.unsolvable)  //当前距离无法射击
+    {
+      // 无法射击也要去调节云台的朝向，因为云台的yaw必须对准，pitch指向装甲板中心的话，误差倒不大
+      send_command(yaw_target, pitch_target);
+      continue;
+    }
+    pitch_target = trajectory.pitch;  //更新pitch_target
+    // 检查是否符合射击条件
+    if (
+      abs(gimbal.state().pitch - yaw_target) <
+        0.01 &&  //射击条件这里其实也不太清楚，目前限制为当前状态与目标状态的yaw与pitch
+      abs(gimbal.state().yaw - pitch_target) <
+        0.01)  //相差在0.01rad之内（约0.57度），之后肯定需要调
+    {
+      if (shoot_count < shoot_total) {
+        // 符合射击条件，发送射击指令
+        send_command(yaw_target, pitch_target, true);
+        shoot_count++;
+        // 等待一段时间，然后重新开始循环
+        std::this_thread::sleep_for(100ms);
+      }
+    }
+    send_command(yaw_target, pitch_target);  //发送指令移动云台
 
     // Your code end
   }
-
 
   return 0;
 }
